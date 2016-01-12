1856d45fd48ef4670be4fe6dc941de2b98906164
wlan8051 <wlan8051@gmail.com>
2016-01-13 00:17:08 +0800

    layout ready to receive content

---
+++ BaiduMaps_test4.5/app/src/main/java/com/example/w/baidumaps_test45/PointListFragment.java
@@ -0,0 +1,29 @@
+package com.example.w.baidumaps_test45;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PointListFragment extends Fragment {
+
+
+    public PointListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_point_list, container, false);
+    }
+
+}
